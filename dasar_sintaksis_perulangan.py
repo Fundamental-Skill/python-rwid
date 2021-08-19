6374617c1d0c90c6040acd4df39588d25da9f9cf
ade.ristanto@gmail.com <56842597-Ade>
2021-08-19 21:58:45 +0700

    2. Perulangan (for)

---
+++ dasar_sintaksis_perulangan.py
@@ -0,0 +1,14 @@
+"""
+Program perulangan membaca buku
+"""
+
+jumlah_buku = 10
+print("Perintah Ibu membaca buku")
+jumlah_buku_dibaca = 0
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}")
+
+for hitung in range(0, jumlah_buku):
+    print(f"Buku ke - {jumlah_buku_dibaca + 1} sudah dibaca")
+    jumlah_buku_dibaca += 1
+
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}")
