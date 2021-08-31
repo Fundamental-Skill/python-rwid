ba6fb19a1cdb7932c6fd0027373ec4dcf314c540
ade.ristanto@gmail.com <56842597-Ade>
2021-08-31 21:23:46 +0700

    4. Dasar sintaksis List part 1

---
+++ dasar_sintaksis_tipe_data_list.py
@@ -0,0 +1,16 @@
+daftar_buku = ['Seven Habits', 'How To Influence People', 'First Things First']
+# print("Tampilkan variable daftar_buku")
+# print(daftar_buku)
+#
+# for buku in daftar_buku:
+#     print(buku)
+#
+# print(daftar_buku[0])
+
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+# Append untuk menambah List dibelakang
+daftar_buku.append("Extends")
+
+print(daftar_buku)
